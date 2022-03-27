0948a3f02896ba2a92d39619c4341343a05402e4
Fernando Lira <it.fernandolira@gmail.com>
2022-03-27 21:48:31 +0100

    Binding to Directive Properties

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit{
+
+  @Input() defaultColor: string = "transparent" ;
+  @Input() highlightColor: string = "blue";
 
   //Renderer alternative if we just want to change some style property
   @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
@@ -14,17 +17,17 @@ export class HighlightDirective implements OnInit{
     //this.elementRef.nativeElement.style.backgroundColor = "green";
     //Better approach
     //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
-    
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseenter(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
-    this.backgroundColor = "blue";
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent');
-    this.backgroundColor = "transparent";
+    this.backgroundColor = this.defaultColor;
   }
 
 
