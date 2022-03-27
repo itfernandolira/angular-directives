f7515a8f8fbda7bfe9b8e7236342b357e6cc1145
Fernando Lira <it.fernandolira@gmail.com>
2022-03-27 21:34:02 +0100

    Using HostBinding to Bind to Host Properties

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit{
+
+  //Renderer alternative if we just want to change some style property
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
@@ -11,14 +14,17 @@ export class HighlightDirective implements OnInit{
     //this.elementRef.nativeElement.style.backgroundColor = "green";
     //Better approach
     //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+    
   }
 
   @HostListener('mouseenter') mouseenter(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+    //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','blue');
+    this.backgroundColor = "blue";
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent');
+    //this.renderer.setStyle(this.elementRef.nativeElement,'background-color','transparent');
+    this.backgroundColor = "transparent";
   }
 
 
